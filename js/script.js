ec91424da12b4781661ef7521cca3fccb02dabb3
emmepi92 <mariapiacoccioli@live.it>
2021-06-29 13:54:20 +0200

    vue.js solo

---
+++ js/script.js
@@ -0,0 +1,31 @@
+Vue.config.devtools = true;
+  
+const add = new Vue (
+    {
+        el: '#app',
+        data: {
+            taskList: ['fare la spesa','prelevare al bancomat', 'comprare pappe a Pupi'],
+            newTask: ''
+        },
+        mounted () {
+            this.getFocus();
+        },
+        methods: {
+            addTask: function () {
+                if (this.newTask !== '') {
+                    this.taskList.push(this.newTask);
+                }
+                this.newTask = '';
+                getFocus();
+            },
+            deleteTask: function(index) {
+                this.taskList.splice(index,1);
+            },
+            getFocus: function() {
+                const element = document.getElementById('input-task');
+                element.focus();
+            }
+
+        }
+    }
+)
